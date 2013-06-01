11f7cb1aaf8b1bd3ed84abb6758323f6b0db1efd
Weien Wang <weienw@gmail.com>
2013-05-31 20:17:41 -0500

    Fix Map View navigation (properly set controller as map view delegate)

--- TopPlaces/MapViewController.m
+++ TopPlaces/MapViewController.m
@@ -43,49 +43,42 @@
 
 //sets up preview image within annotation for mapView of photos
 - (MKAnnotationView *) mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
-    NSLog(@"Enter viewForAnnotation with %@", [annotation description]);
     MKAnnotationView *aView = [mapView dequeueReusableAnnotationViewWithIdentifier:@"MapVC"]; //instead of always creating a new one
     if (!aView) {
         aView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"MapVC"]; //allocate if none existing
         aView.canShowCallout = YES;
-        aView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];        
+        aView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure]; //must set accessoryview here, not in didSelectAnnotationView
         if (self.mapViewControllerDelegate) //check for image delegate before allocating space
         aView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)]; //30x30 is good size for an annotation image
     }
     else {
         aView.annotation = annotation;
     }
-    NSLog(@"aView is %@", [aView description]);
     return aView;
 }
 
-//sent when pin is selected on map; set calloutview image (as allocated in mapView:viewForAnnotation) at this point (instead of when pin is generated)
+//sent when pin is selected on map; set calloutview image at this point (instead of when pin is generated)
 - (void) mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)aView {
     UIImage *image = [self.mapViewControllerDelegate mapViewController:self imageForAnnotation:aView.annotation];
     [(UIImageView*) aView.leftCalloutAccessoryView setImage:image];
 }
 
+//initiate segues to phototableview/photoview via map
 - (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
-    NSLog(@"Button tapped!");
     AnnotationUtil *au = view.annotation;
-    if ([au.mappedItem valueForKey:@"place_type"]!=nil)
-    {
-        NSLog(@"Is Place segue, segue to show photos list, place_type is %@", [au.mappedItem valueForKey:@"place_type"]);
+    if ([au.mappedItem valueForKey:@"place_type"]!=nil) //using "place_type" (unique to place-object) to determine which segue to perform
         [self performSegueWithIdentifier:@"showAnnotationPlace" sender:au.mappedItem];
-    }
-    else {
-        NSLog(@"Is Photo segue, segue to show image");
+    else
         [self performSegueWithIdentifier:@"showAnnotationPhoto" sender:au.mappedItem];
-    }
 }
 
 - (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"showAnnotationPlace"]) {
-        NSLog(@"prepare for showAnnotationPlace");
+        //NSLog(@"prepare for showAnnotationPlace");
         [segue.destinationViewController setItems:[FlickrFetcher photosInPlace:sender maxResults:50]];
     }
     if ([segue.identifier isEqualToString:@"showAnnotationPhoto"]) {
-        NSLog(@"prepare for showAnnotationPhoto");
+        //NSLog(@"prepare for showAnnotationPhoto");
         [segue.destinationViewController setPhoto:sender];
     }
 }
@@ -99,7 +92,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.mapView.delegate = self; //allows implementing annotation view features
+    self.mapView.delegate = self; //very important; check to make sure storyboard/nib also reflects this delegate assignment
 }
 
 - (void)viewDidUnload
